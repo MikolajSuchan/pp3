6e9e79b01bbd7aadc84adf997a179fa6f749af35
MikolajSuchan <225887@studnet.uek.krakow.pl>
2023-10-17 14:37:57 +0200

    2023-17-10

---
+++ 02-ClassesAndObjects/Speedlimit.java
@@ -0,0 +1,11 @@
+public class Speedlimit {
+    public static void main(String[] args){
+        int speed= 140;
+
+    if (speed>=40 && speed<=140){
+    System.out.println("Speed is valid: "+true);
+    } else {
+    System.out.println("Speed is valid: "+false);
+    }
+    }
+}
