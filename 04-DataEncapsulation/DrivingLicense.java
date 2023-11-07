1341ea61efcc4df5db69b39ecfa4de40765f1bf7
Mikołaj Suchan <225887@student.uek.krakow.pl>
2023-11-07 12:40:56 +0100

    MS

---
+++ 04-DataEncapsulation/DrivingLicense.java
@@ -0,0 +1,10 @@
+public class DrivingLicense {
+    private String name;
+    private String surename;
+    private String addesss;
+    private String postal_code;
+    private String city;
+    private String dl_number;
+    private String year_of_issue;
+    private String license_cathegory;
+}
---
+++ 04-DataEncapsulation/DrivingLicenseTest.java
@@ -0,0 +1,13 @@
+public class DrivingLicenseTest {
+    public static void main(String[] args) {
+        DrivingLicense dl=new DrivingLicense();
+        dl.name="Honorata";
+        dl.surename="Malinowska";
+        dl.addesss="";
+        dl.postal_code="";
+        dl.city="Krakow";
+        dl.dl_number="12341212";
+        dl.year_of_issue="2022";
+        dl.license_cathegory="B";
+    }
+}
