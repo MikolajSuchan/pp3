bedacec3d23c6d4d553675b68ec8b1664356e3a1
Mikołaj Suchan <114940113+MikolajSuchan@users.noreply.github.com>
2023-11-07 14:48:22 +0100

    Add files via upload

---
+++ StringxdTest.java
@@ -0,0 +1,9 @@
+public class StringxdTest {
+    public static void main(String[] args) {
+        Stringxd str= new Stringxd();
+        str.setSentance("Have a nice day!");
+
+    str.display();
+
+    }
+}
